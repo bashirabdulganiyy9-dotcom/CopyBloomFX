5c19d4889003e753a1f693dd309767c0cdc70367
bashirabdulganiyy9-dotcom <bashirabdulganiyy9@gmail.com>
2026-01-31 23:10:47 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -2,5 +2,5 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crypto_platform.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crypto_platform.railway_settings')
 application = get_wsgi_application()
